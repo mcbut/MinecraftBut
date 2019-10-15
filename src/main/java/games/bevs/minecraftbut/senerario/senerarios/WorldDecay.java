2242834c78379b6b5d4503a57e903b36394c8e65
Heath Logan Campbell <github@heathlogancampbell.com>
2019-10-16 11:05:29 +1300

    add world decay

---
+++ src/main/java/games/bevs/minecraftbut/senerario/senerarios/WorldDecay.java
@@ -0,0 +1,53 @@
+package games.bevs.minecraftbut.senerario.senerarios;
+
+import org.apache.commons.lang3.math.NumberUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import games.bevs.minecraftbut.commons.Console;
+import games.bevs.minecraftbut.commons.XMaterial;
+import games.bevs.minecraftbut.commons.utils.CC;
+import games.bevs.minecraftbut.commons.utils.MathUtils;
+import games.bevs.minecraftbut.senerario.Senerario;
+import games.bevs.minecraftbut.senerario.options.Optional;
+import games.bevs.minecraftbut.world.ButWorld;
+import net.md_5.bungee.api.ChatColor;
+
+public class WorldDecay extends Senerario
+{
+	@Optional
+	private long decaysPerSecond = 10l;
+	
+	public WorldDecay(ButWorld butWorld) 
+	{
+		super("The Lava is rising", butWorld, Material.BRICK, new String[] {"Every minute (or the amount of time you set) the lava level will riase"});
+	}
+
+	
+	@Override
+	public void onStart()
+	{
+		this.repeat(() -> {
+			for(int i = 0; i < decaysPerSecond; i++)
+			{
+				int ranX = MathUtils.getRandom().nextInt(this.getButWorld().getMaxLocation().getBlockX() - this.getButWorld().getMinLocation().getBlockX()) + this.getButWorld().getMinLocation().getBlockX();
+				int ranZ = MathUtils.getRandom().nextInt(this.getButWorld().getMaxLocation().getBlockZ() - this.getButWorld().getMinLocation().getBlockZ()) + this.getButWorld().getMinLocation().getBlockZ();
+				Block block = this.getButWorld().getWorld().getHighestBlockAt(ranX, ranZ);
+				
+				if(block.getType() == Material.BEDROCK)
+					continue;
+				
+				block.setType(Material.AIR);
+			}
+		}, 20l * 1);
+	}
+	
+	@Override
+	public void onFinish()
+	{
+		
+	}
+}
