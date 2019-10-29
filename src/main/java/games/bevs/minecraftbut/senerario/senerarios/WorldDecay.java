01c1561c076ef1333a6671f6d61dc582840d7eea
Heath Logan Campbell <github@heathlogancampbell.com>
2019-10-30 11:32:04 +1300

    support skyblock?

--- src/main/java/games/bevs/minecraftbut/senerario/senerarios/WorldDecay.java
+++ src/main/java/games/bevs/minecraftbut/senerario/senerarios/WorldDecay.java
@@ -37,6 +37,7 @@ public class WorldDecay extends Senerario
 				int ranX = MathUtils.getRandom().nextInt(this.getButWorld().getMaxLocation().getBlockX() - this.getButWorld().getMinLocation().getBlockX()) + this.getButWorld().getMinLocation().getBlockX();
 				int ranZ = MathUtils.getRandom().nextInt(this.getButWorld().getMaxLocation().getBlockZ() - this.getButWorld().getMinLocation().getBlockZ()) + this.getButWorld().getMinLocation().getBlockZ();
 				Block block = this.getButWorld().getWorld().getHighestBlockAt(ranX, ranZ);
+				if(block == null) continue;
 				block = block.getRelative(BlockFace.DOWN);
 				
 				if(block.getType() == Material.BEDROCK)
