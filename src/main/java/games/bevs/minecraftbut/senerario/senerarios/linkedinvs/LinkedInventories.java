348c62a724b57ba8c51c6622acc24dd71010cc39
Heath Logan Campbell <github@heathlogancampbell.com>
2019-11-19 21:32:04 +1300

    linked inventory

--- src/main/java/games/bevs/minecraftbut/senerario/senerarios/linkedinvs/LinkedInventories.java
+++ src/main/java/games/bevs/minecraftbut/senerario/senerarios/linkedinvs/LinkedInventories.java
@@ -2,19 +2,19 @@ package games.bevs.minecraftbut.senerario.senerarios.linkedinvs;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.craftbukkit.v1_8_R3.entity.CraftHumanEntity;
-import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryEvent;
-import org.bukkit.event.player.PlayerInventoryEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.inventory.InventoryDragEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerItemConsumeEvent;
+import org.bukkit.event.player.PlayerPickupItemEvent;
 
 import games.bevs.minecraftbut.senerario.Senerario;
 import games.bevs.minecraftbut.world.ButWorld;
-import net.minecraft.server.v1_8_R3.PlayerInventory;
 
 /**
  * All players share an inventory
@@ -40,17 +40,72 @@ public class LinkedInventories extends Senerario
 		
 	}
 	
-	@EventHandler
-	public void onInventory(InventoryClickEvent e)
+	public void updateInventoryForAll(Player player, long delay)
 	{
-		Player player = (Player) e.getWhoClicked();
 		this.delay(() -> {
 			for(Player onlinePlayer : Bukkit.getOnlinePlayers())
 			{
 				if(player == onlinePlayer) continue;
 				onlinePlayer.getInventory().setArmorContents(player.getInventory().getArmorContents());
 				onlinePlayer.getInventory().setContents(player.getInventory().getContents());
+				onlinePlayer.updateInventory();
 			}
-		}, 1l);
+		}, delay);
+	}
+	
+	@EventHandler
+	public void onDrop(PlayerDropItemEvent e)
+	{
+		Player player = e.getPlayer();
+		this.updateInventoryForAll(player, 2);
+	}
+	
+	@EventHandler
+	public void onPick(PlayerPickupItemEvent e)
+	{
+		Player player = e.getPlayer();
+		this.updateInventoryForAll(player, 2);
+	}
+	
+	@EventHandler
+	public void onPlace(BlockPlaceEvent e)
+	{
+		Player player = e.getPlayer();
+		this.updateInventoryForAll(player, 2);
+	}
+	
+	@EventHandler
+	public void onPlace(PlayerItemConsumeEvent e)
+	{
+		Player player = e.getPlayer();
+		this.updateInventoryForAll(player, 2);
+	}
+	
+	@EventHandler
+	public void onInteract(PlayerInteractEvent e)
+	{
+		Player player = e.getPlayer();
+		this.updateInventoryForAll(player, 2);
+	}
+	
+	@EventHandler
+	public void onDrag(InventoryDragEvent e)
+	{
+		Player player = (Player) e.getWhoClicked();
+		this.updateInventoryForAll(player, 2);
+	}
+	
+	@EventHandler
+	public void onDeath(PlayerDeathEvent e)
+	{
+		Player player = e.getEntity();
+		this.updateInventoryForAll(player, 2);
+	}
+	
+	@EventHandler
+	public void onInventory(InventoryClickEvent e)
+	{
+		Player player = (Player) e.getWhoClicked();
+		this.updateInventoryForAll(player, 2);
 	}
 }
